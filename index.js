bd60225630f0fa4941447c21e701ee35e8e412e4
tragdir <msiguenza1337@gmail.com>
2022-02-01 12:54:08 -0500

    first attempt

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
+
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://tragdir:<password>@techdivedb.olqoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
