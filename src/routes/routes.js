1a4636094a6fe963c8270819fe887cf25eb40809
tragdir <msiguenza1337@gmail.com>
2022-02-01 15:57:26 -0500

    module4 tests complete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,12 @@
-import { addnewProduct } from '../controllers/controllers';
+import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers';
 
 const routes = (app) => {
     app.route('/products');
+    app.get(getProducts);
     app.post(addnewProduct);
+    
+    app.route('/products/:ProductID');
+    app.get(getProductWithID);
 }
 
 export default routes;
